55540776a6d0c86d14875dde1b0453894f968566
pratistha04 <sanwalpratistha@gmail.com>
2025-02-20 14:52:28 +0530

    day-1 fs promises

--- d1/p6.js
+++ d1/p6.js
@@ -1,13 +1,13 @@
 const fs=require('fs');
 const write=(data)=>{
     
-    fs.writeFile("./data.txt","utf8",(err,data)=>{
+    fs.writeFile("./mydir/data.txt","utf8",(err,data)=>{
         if(err)
             console.log("ERROR",err);
         else
         console.log("SUCCESS");
     });
 }
-console.log("before")
+//console.log("before")
 write("i am new data");
-console.log("after")
+//console.log("after")
